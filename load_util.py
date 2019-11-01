48f46344b1b44a5d4c6e6508fa182cb30476c6c6
Eugene Palovcak <eugene.palovcak@gmail.com>
2019-11-01 16:25:47 -0700

    Basic test of data loader - need to preprocess so loader doesn't fail on PDB parse

--- load_util.py
+++ load_util.py
@@ -17,4 +17,49 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from torch_geometric.data import DataLoader
+from torch_geometric.data import Dataset
+from torch_geometric.data import Data
+from prody import parsePDB
 
+from scipy.spatial.distance import squareform
+from scipy.spatial.distance import pdist
+
+import glob
+
+def load_protein_structure(path):
+    """ Load a protein structure and return coords as Nx3 array """
+    pdb = parsePDB(path).select('name CA CB')
+    coords = pdb.getCoords()
+    coords -= coords.mean(axis=0)
+    return coords
+
+class ProteinDataset(Dataset):
+    """ Makes a pytorch-geometric dataset of point clouds from a 
+    directory of PDB files """
+
+    def __init__(self, pdb_dir, transform=None, pre_transform=None):
+        super(ProteinDataset, self).__init__(pdb_dir, transform, pre_transform)
+        self.pdb_files = glob.glob(pdb_dir + "/*.pdb")
+
+    def __len__(self):
+        return len(self.pdb_files)
+
+    def _download(self):
+        pass
+  
+    def _process(self):
+        pass
+
+    def get(self, idx):
+        pdb_file = self.pdb_files[idx]
+        coords = load_protein_structure(pdb_file)
+        data = Data(x=coords)
+        return data
+
+
+if __name__=="__main__":
+    D = ProteinDataset("./dompdb")
+    loader = DataLoader(D, batch_size=16)
+    for i in loader:
+       print(i)
